3768ec1c94aeb58b3c1feaf2726ba1026ae8f91c
GlennnM <h>
2023-01-25 01:38:07 -0500

    forward args
    Former-commit-id: 43b2d86828073e996ef2478b1c1e86e5b82c73eb

--- src/java/xyz/hydar/flash/mp/S4Server.java
+++ src/java/xyz/hydar/flash/mp/S4Server.java
@@ -1080,11 +1080,11 @@ public class S4Server extends ServerContext{
 		FEB_16_2022=LocalDateTime.ofInstant(c.toInstant(),ZoneId.of("GMT-8"));
 		byte[] tmp1,tmp2,tmp3;
 		try {
-			tmp1 = Files.readAllBytes(Paths.get("./bot.bin"));
-			tmp2 = Files.readAllBytes(Paths.get("./vs.bin"));
-			tmp3 = Files.readAllBytes(Paths.get("./deadtab.bin"));
+			tmp1 = Files.readAllBytes(Paths.get("./bot_normal.bin"));
+			tmp2 = Files.readAllBytes(Paths.get("./bot_vs.bin"));
+			tmp3 = Files.readAllBytes(Paths.get("./bot_deadtab.bin"));
 		} catch (Exception e) {
-			System.out.println("warning: bot data not found(bot.bin, vs.bin, deadtab.bin)");
+			System.out.println("warning: bot data not found(bot_normal.bin, bot_vs.bin, bot_deadtab.bin)");
 			tmp1=tmp2=tmp3=new byte[0];
 		}
 		bot=tmp1;
