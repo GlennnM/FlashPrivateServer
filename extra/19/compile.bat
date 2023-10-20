d20b7e809f7eaaf6663de6bbda50424f63559fcf
GlennnM <77253453+GlennnM@users.noreply.github.com>
2023-10-19 23:27:20 -0400

    remove preview scripts

--- extra/19/compile.bat
+++
@@ -1,2 +0,0 @@
-javac ./src/java/xyz/hydar/net/*.java ./src/java/xyz/hydar/flash/util/*.java ./src/java/xyz/hydar/flash/*.java -cp ./classes -d ./classes --release 19 --enable-preview
-java --enable-preview -cp ./classes xyz.hydar.flash.FlashLauncher %*
--- extra/19/package.bat
+++
@@ -1,4 +0,0 @@
-cd ../
-javac ./src/java/xyz/hydar/net/*.java ./src/java/xyz/hydar/flash/util/*.java ./src/java/xyz/hydar/flash/*.java -cp ./classes -d ./classes --release 19 --enable-preview
-cd ./classes/
-jar -cfe ../FlashServer.jar xyz.hydar.flash.FlashLauncher *
